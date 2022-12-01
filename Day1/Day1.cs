694d9fa81552340cab39ef3324b29236aa289df2
est5 <ghostrekon5500@ya.ru>
2022-12-01 12:51:11 +0300

    Day 1 - part 1

---
+++ Day1/Day1.cs
@@ -0,0 +1,18 @@
+﻿string[] lines = System.IO.File.ReadAllLines("./input.txt");
+
+// int elfCount = 0;
+int maxValue = 0;
+int tempValue = 0;
+foreach (string line in lines)
+{
+    try
+    {
+        tempValue += Convert.ToInt32(line);
+    }
+    catch (Exception)
+    {
+        maxValue = maxValue > tempValue ? maxValue : tempValue;
+        tempValue = 0;
+    }
+}
+Console.WriteLine(maxValue);
