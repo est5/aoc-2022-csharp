16f86e718ca441dd491d076f8c7e28dd81e12607
est5 <ghostrekon5500@ya.ru>
2022-12-01 13:01:42 +0300

    Day 1 - part 2

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -1,18 +1,46 @@
 ﻿string[] lines = System.IO.File.ReadAllLines("./input.txt");
 
-// int elfCount = 0;
-int maxValue = 0;
-int tempValue = 0;
-foreach (string line in lines)
+PartOne(lines);
+PartTwo(lines);
+
+static void PartOne(string[] lines)
 {
-    try
+    Console.WriteLine("Part one");
+    int maxValue = 0;
+    int tempValue = 0;
+    foreach (string line in lines)
     {
-        tempValue += Convert.ToInt32(line);
+        try
+        {
+            tempValue += Convert.ToInt32(line);
+        }
+        catch (Exception)
+        {
+            maxValue = maxValue > tempValue ? maxValue : tempValue;
+            tempValue = 0;
+        }
     }
-    catch (Exception)
-    {
-        maxValue = maxValue > tempValue ? maxValue : tempValue;
-        tempValue = 0;
-    }
+    Console.WriteLine(maxValue);
 }
-Console.WriteLine(maxValue);
+
+static void PartTwo(string[] lines)
+{
+    Console.WriteLine("Part two");
+
+    List<int> elfsTotal = new();
+    int total = 0;
+    foreach (string line in lines)
+    {
+        try
+        {
+            total += Convert.ToInt32(line);
+        }
+        catch (Exception)
+        {
+            elfsTotal.Add(total);
+            total = 0;
+        }
+    }
+    elfsTotal.Sort((int a, int b) => b - a);
+    System.Console.WriteLine($"{elfsTotal[0]}, {elfsTotal[1]}, {elfsTotal[2]}\nTotal {elfsTotal[0] + elfsTotal[1] + elfsTotal[2]}");
+}
