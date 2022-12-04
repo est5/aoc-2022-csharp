49ad296d3165b46f6673577fdde474575c630993
est5 <ghostrekon5500@ya.ru>
2022-12-04 13:01:16 +0300

    Day 4 - part 2

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -1,9 +1,12 @@
-﻿string[] lines = System.IO.File.ReadAllLines("./test.txt");
+﻿string[] lines = System.IO.File.ReadAllLines("./input.txt");
 
 PartOne(lines);
+PartTwo(lines);
+
 
 void PartOne(string[] lines)
 {
+    System.Console.WriteLine("Part one");
     int total = 0;
     foreach (var line in lines)
     {
@@ -18,6 +21,30 @@ void PartOne(string[] lines)
         }
         else if (Convert.ToInt32(firstPairtNums[0]) >= Convert.ToInt32(secondPairtNums[0]) &&
              Convert.ToInt32(firstPairtNums[1]) <= Convert.ToInt32(secondPairtNums[1]))
+        {
+            total++;
+        }
+    }
+    Console.WriteLine(total);
+}
+
+void PartTwo(string[] lines)
+{
+    System.Console.WriteLine("Part two");
+    int total = 0;
+    foreach (var line in lines)
+    {
+        var pairs = line.Split(',');
+        var firstPairtNums = pairs[0].Split('-');
+        var secondPairtNums = pairs[1].Split('-');
+
+        if (Convert.ToInt32(secondPairtNums[1]) >= Convert.ToInt32(firstPairtNums[0]) &&
+            Convert.ToInt32(secondPairtNums[0]) <= Convert.ToInt32(firstPairtNums[1]))
+        {
+            total++;
+        }
+        else if (Convert.ToInt32(firstPairtNums[1]) >= Convert.ToInt32(secondPairtNums[0]) &&
+             Convert.ToInt32(firstPairtNums[0]) <= Convert.ToInt32(secondPairtNums[1]))
         {
             total++;
         }
